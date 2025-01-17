c54dbf422ff8af61a33b4f780910ae02ad89710a
aandri.sap <62608079+bonjer-26@users.noreply.github.com>
2025-01-17 10:40:50 +1000

    Update helper.py

--- helper.py
+++ helper.py
@@ -11,12 +11,12 @@ def extract(input, t, x):
     reshape = [t.shape[0]] + [1] * (len(shape) - 1)
     return out.reshape(*reshape)
 
-def make_beta_schedule(schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2):
-    if schedule == 'linear':
+def make_beta_schedule(schedule='cosine', n_timesteps=500, start=1e-5, end=1e-2):
+    if schedule == "cosine":
+        t = torch.linspace(0, n_timesteps, n_timesteps + 1) / n_timesteps
+        alphas = (torch.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2)
+        alphas = alphas / alphas[0]
+        betas = 1 - (alphas[1:] / alphas[:-1])
+    elif schedule == 'linear':
         betas = torch.linspace(start, end, n_timesteps)
-    elif schedule == "quad":
-        betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
-    elif schedule == "sigmoid":
-        betas = torch.linspace(-6, 6, n_timesteps)
-        betas = torch.sigmoid(betas) * (end - start) + start
     return betas
